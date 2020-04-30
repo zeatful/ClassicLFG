e0eba363177245f68919ba066a265a19e3796b31
Eric Foster <efoster@harriscomputer.com>
2020-04-30 01:38:16 -0400

    move roles and instances declarations to top level

--- core/core.lua
+++ core/core.lua
@@ -8,7 +8,34 @@ local AceComm = LibStub("AceComm-3.0")
 --  Global Variables
 --------------------
 local parentFrame, appropriateLevelCheckbox, roleDropDown, instanceDropDown, queueButton
-local roles, instances
+
+-- table of roles
+local roles = {}
+roles[0] = "Tank"
+roles[1] = "Healer"
+roles[2] = "Dps"
+
+-- table of instances : key, name, minimum level and maximum level
+local instances = {}
+instances[0] = { name = "Ragefire Chasm", minLevel = 13, maxLevel = 18}
+instances[1] = { name = "Wailing Caverns", minLevel = 17, maxLevel = 24}
+instances[2] = { name = "The Deadmines", minLevel = 17, maxLevel = 26}
+instances[3] = { name = "Shadowfang Keep", minLevel = 22, maxLevel = 30}
+instances[4] = { name = "Blackfathom Deeps", minLevel = 24, maxLevel = 32}
+instances[5] = { name = "The Stockade", minLevel = 24, maxLevel = 32}
+instances[6] = { name = "Gnomeregan", minLevel = 29, maxLevel = 38}
+instances[7] = { name = "Razorfen Kraul", minLevel = 29, maxLevel = 38}
+instances[8] = { name = "The Scarlet Monastery", minLevel = 34, maxLevel = 45}
+instances[9] = { name = "Razorfen Downs", minLevel = 37, maxLevel = 46}
+instances[10] = { name = "Uldaman", minLevel = 41, maxLevel = 51}
+instances[11] = { name = "Zul’Farrak", minLevel = 42, maxLevel = 46}
+instances[12] = { name = "Maraudon", minLevel = 46, maxLevel = 55}
+instances[13] = { name = "Temple of Atal’Hakkar", minLevel = 50, maxLevel = 56}
+instances[14] = { name = "Blackrock Depths", minLevel = 52, maxLevel = 60}
+instances[15] = { name = "Lower Blackrock Spire", minLevel = 55, maxLevel = 60}
+instances[16] = { name = "Dire Maul", minLevel = 55, maxLevel = 60}
+instances[17] = { name = "Stratholme", minLevel = 58, maxLevel = 60}
+instances[18] = { name = "Scholomance", minLevel = 58, maxLevel = 60}
 
 --------------------
 --  Functions
@@ -58,39 +85,11 @@ appropriateLevelCheckbox:SetLabel("Only show appropriate instances based on leve
 appropriateLevelCheckbox:SetCallback("OnMouseUp", SetInstancesForDropDown())
 parentFrame:AddChild(appropriateLevelCheckbox)
 
--- table of roles
-roles = {}
-roles[0] = "Tank"
-roles[1] = "Healer"
-roles[2] = "Dps"
-
 -- Role DropDown - Tank, Healer, Dps
 roleDropDown = AceGUI:Create("Dropdown")
 roleDropDown:SetText("Select Role")
 roleDropDown:SetList(roles)
 parentFrame:AddChild(roleDropDown)
-
--- table of instances : key, name, minimum level and maximum level
-instances = {}
-instances[0] = { name = "Ragefire Chasm", minLevel = 13, maxLevel = 18}
-instances[1] = { name = "Wailing Caverns", minLevel = 17, maxLevel = 24}
-instances[2] = { name = "The Deadmines", minLevel = 17, maxLevel = 26}
-instances[3] = { name = "Shadowfang Keep", minLevel = 22, maxLevel = 30}
-instances[4] = { name = "Blackfathom Deeps", minLevel = 24, maxLevel = 32}
-instances[5] = { name = "The Stockade", minLevel = 24, maxLevel = 32}
-instances[6] = { name = "Gnomeregan", minLevel = 29, maxLevel = 38}
-instances[7] = { name = "Razorfen Kraul", minLevel = 29, maxLevel = 38}
-instances[8] = { name = "The Scarlet Monastery", minLevel = 34, maxLevel = 45}
-instances[9] = { name = "Razorfen Downs", minLevel = 37, maxLevel = 46}
-instances[10] = { name = "Uldaman", minLevel = 41, maxLevel = 51}
-instances[11] = { name = "Zul’Farrak", minLevel = 42, maxLevel = 46}
-instances[12] = { name = "Maraudon", minLevel = 46, maxLevel = 55}
-instances[13] = { name = "Temple of Atal’Hakkar", minLevel = 50, maxLevel = 56}
-instances[14] = { name = "Blackrock Depths", minLevel = 52, maxLevel = 60}
-instances[15] = { name = "Lower Blackrock Spire", minLevel = 55, maxLevel = 60}
-instances[16] = { name = "Dire Maul", minLevel = 55, maxLevel = 60}
-instances[17] = { name = "Stratholme", minLevel = 58, maxLevel = 60}
-instances[18] = { name = "Scholomance", minLevel = 58, maxLevel = 60}
 
 -- Instance DropDown, IE: Scholomance, Wailing Caverns, etc...
 instanceDropDown = AceGUI:Create("Dropdown")
